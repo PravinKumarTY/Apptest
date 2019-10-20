fcaf86663f468105ab006bc84ec772386d10ac92
PravinKumarTY <56624466+PravinKumarTY@users.noreply.github.com>
2019-10-20 15:54:35 +0530

    Update HelloJava.java

--- spring-boot-demo/src/main/java/com/tyss/springbootdemo/config/HelloJava.java
+++ spring-boot-demo/src/main/java/com/tyss/springbootdemo/config/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello java");
 		System.out.println("This is second commit part");
+		System.out.println("This is changes from git");
 	}
 
 }
